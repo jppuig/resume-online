b8a4a377ef8d8ea92e1ed8e46905e1571f705091
jppuig <jppuigpesce@gmail.com>
2023-08-08 09:48:03 -0300

    Add tailwind

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  tailwind: true,
   ignoredRouteFiles: ["**/.*"],
   server:
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
