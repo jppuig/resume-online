b8787b22085f0f3556e05d114ec63e25c53d731c
jppuig <jppuigpesce@gmail.com>
2023-08-07 22:42:35 -0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    root: true,
+    extends: [
+      "@remix-run/eslint-config",
+      "@remix-run/eslint-config/node",
+      "prettier",
+    ],
+  };
+  
