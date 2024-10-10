d5b68d3420cdf3e776121ebd995fd7e463286003
jppuig <jppuigpesce@gmail.com>
2024-10-10 14:42:41 -0300

    Added tech area

--- app/routes/education._index.tsx
+++ app/routes/education._index.tsx
@@ -52,7 +52,7 @@ export default function ResumeIndexRoute() {
     },
     {
       title: "Areas",
-      content: "Machine learning, Blockchain & DApps, Web development, Mobile Development",
+      content: "Machine learning, Blockchain & DApps, Web development, Mobile Development, Artificial Intelligence",
     },
     {
       title: "Tools",
